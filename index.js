f3a35a048a8a1ce8cb717c0437e4b03a0ea5258e
Angelo Laub <angelolaub@gmail.com>
2019-04-26 13:36:06 +0200

    remove /

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const chains = [
   },
   {
     name: 'Kylin Testnet',
-    httpEndpoint: 'https://api.kylin.alohaeos.com/',
+    httpEndpoint: 'https://api.kylin.alohaeos.com',
     chainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191',
     contract: 'priveosrules',    
   }
